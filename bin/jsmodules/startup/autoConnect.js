019eebb2ef46c606e8db79c7c536240139a5d54c
Juha Hyvärinen <juhahyva@gmail.com>
2011-02-08 14:54:09 +0200

    Changed connection scripts to use Login-function from TundraLogicModule/client.cpp, so we can easily chance which protocol is used for auto and fallbackconnection. Also added functionality to autoConnect that local server don't try to run script.

--- bin/jsmodules/startup/autoConnect.js
+++ bin/jsmodules/startup/autoConnect.js
@@ -1,3 +1,11 @@
 var randomnumber=Math.floor(Math.random()*100)
+//console.ExecuteCommand("connect(chiru.cie.fi,2345,Pertti"+randomnumber.toString()+")" )
+function AutoConnect()
+{
+    var isserver = server.IsRunning();
+    if(isserver==false){
+        client.Login("chiru.cie.fi",2345,"Guest"+randomnumber.toString()," ","udp");
+    }
+}
 
-console.ExecuteCommand("connect(chiru.cie.fi,2345,Pertti"+randomnumber.toString()+")" )
+frame.DelayedExecute(0.1).Triggered.connect(this,AutoConnect);

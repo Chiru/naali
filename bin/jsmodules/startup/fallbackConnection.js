696cd68bdf46a1881c60d7e0e0b54315caab3b69
Jarkko Vatjus-Anttila <jvatjusanttila@gmail.com>
2011-02-24 11:54:01 +0200

    Switched default fallback connection protocol to UDP

--- bin/jsmodules/startup/fallbackConnection.js
+++ bin/jsmodules/startup/fallbackConnection.js
@@ -3,5 +3,5 @@ client.FallbackConnection.connect(fallbackConnection);
 function fallbackConnection()
 {
     //const QString& address, unsigned short port, const QString& username, const QString& password, const QString &protocol = QString()
-    client.Login("localhost",2345,"user","","tcp");
+    client.Login("localhost",2345,"user","","udp");
 }

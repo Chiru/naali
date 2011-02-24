f6f241ad0702b0def8167127a5ed8e3fb7a2deb7
Erno Kuusela <erno@playsign.net>
2011-02-24 14:50:29 +0200

    check for server.IsAboutToStart() in addition to server.IsRunning() in autoConnect.js

--- bin/jsmodules/startup/autoConnect.js
+++ bin/jsmodules/startup/autoConnect.js
@@ -2,10 +2,10 @@ var randomnumber=Math.floor(Math.random()*100)
 //console.ExecuteCommand("connect(chiru.cie.fi,2345,Pertti"+randomnumber.toString()+")" )
 function AutoConnect()
 {
-    var isserver = server.IsRunning();
+    var isserver = server.IsRunning() || server.IsAboutToStart();
     if(isserver==false){
         client.Login("chiru.cie.fi",2345,"Guest"+randomnumber.toString()," ","udp");
     }
 }
 
-frame.DelayedExecute(0.1).Triggered.connect(this,AutoConnect);
+frame.DelayedExecute(1).Triggered.connect(this,AutoConnect);

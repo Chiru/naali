40fcfa9775398118590e1436eb5fa99a5f961e4c
Kari Vatjus-Anttila <karivatj@paju.oulu.fi>
2012-06-19 15:34:33 +0300

    Initial commit

--- src/Core/TundraProtocolModule/InterestManagerDialog.cpp
+++ src/Core/TundraProtocolModule/InterestManagerDialog.cpp
@@ -3,7 +3,7 @@
  *  For conditions of distribution and use, see copyright notice in license.txt
  *
  */
-
+ 
 #include "StableHeaders.h"
 #include "InterestManagerDialog.h"
 #include <QLabel>
--- src/Core/TundraProtocolModule/InterestManagerDialog.h
+++ src/Core/TundraProtocolModule/InterestManagerDialog.h
@@ -2,8 +2,8 @@
  *  Copyright (c) 2011 CIE / University of Oulu, All Rights Reserved
  *  For conditions of distribution and use, see copyright notice in license.txt
  *
- */
-
+ */ 
+ 
 #pragma once
 
 #include <QDialog>

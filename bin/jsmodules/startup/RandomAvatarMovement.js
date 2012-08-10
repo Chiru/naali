813b7ce3ff431fcff301e09141851d49282d7953
Kari Vatjus-Anttila <karivatj@paju.oulu.fi>
2012-08-10 11:41:40 +0300

    Modified RandomAvatarMovement to not use rand() function. Rather use a predefined "randomly" generated string of commands so the path can be reproduced for testing purposes.

--- bin/jsmodules/startup/RandomAvatarMovement.js
+++ bin/jsmodules/startup/RandomAvatarMovement.js
@@ -3,6 +3,10 @@ framework.Scene().SceneAdded.connect(OnSceneAdded);
 var scene = null;
 var avatarEntityId = 0;
 
+var commands = [1,3,1,1,1,1,5,10,6,3,1,1,11,12,8,7,4,3,1,9,10,4,5,11,12,8,4,
+                1,1,1,4,9,10,8,7,5,4,2,1,11,12,5,7,11,12,6,4,3,9,10];
+var index = 0;
+
 function OnSceneAdded(scenename)
 {
     // Get pointer to scene through framework
@@ -15,20 +19,34 @@ function OnEntityCreated(entity, change)
     if (!server.IsRunning() && entity.name == "Avatar" + client.connectionId)
     {
         avatarEntityId = entity.id;
-        frame.Updated.connect(ProduceRandomMovement);
+        frame.DelayedExecute(1.0).Triggered.connect(ProduceRandomMovement);
+//        frame.Updated.connect(ProduceRandomMovement);
     }
 }
 
 function ProduceRandomMovement()
 {
     var entity = scene.GetEntity(avatarEntityId);
+
     if (entity == null)
     {
-        frame.Updated.disconnect(ProduceRandomMovement);
+        frame.DelayedExecute(1.0).Triggered.disconnect(ProduceRandomMovement);
+        //frame.Updated.disconnect(ProduceRandomMovement);
         return;
     }
 
-    var decision = Math.floor(Math.random() * 64);
+    if(index >= commands.length)
+    {
+        index = 0;
+        print("Rewinding");
+    }
+
+    var decision = commands[index];
+    index += 1;
+
+    print("Moving avatar: " + decision);
+    //var decision = Math.floor(Math.random() * 64);
+
     if (decision == 1)
         entity.Exec(2, "Move", "forward");
     if (decision == 2)
@@ -45,4 +63,13 @@ function ProduceRandomMovement()
         entity.Exec(2, "Stop", "right");
     if (decision == 8)
         entity.Exec(2, "Stop", "left");
+    if (decision == 9)
+        entity.Exec(1, "Rotate", "right");
+    if (decision == 10)
+        entity.Exec(1, "StopRotate", "right");
+    if (decision == 11)
+        entity.Exec(1, "Rotate", "left");
+    if (decision == 12)
+        entity.Exec(1, "StopRotate", "left");
+    frame.DelayedExecute(1.0).Triggered.connect(ProduceRandomMovement);
 }
